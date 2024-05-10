336d6c879d169fa0a2075ce69be2fa36127cfd9f
Omsurvase24 <omsurvase24@gmail.com>
2024-05-11 01:14:41 +0530

    final

--- vgsales_app.py
+++ vgsales_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pickle
-import sklearn
 import pandas as pd
 import numpy as np
 
@@ -14,21 +13,32 @@ def user_report():
     NA_Sales = st.sidebar.slider('NA_Sales', 0, 40, 1)
     EU_Sales = st.sidebar.slider('EU_Sales', 0, 40, 1)
     JP_Sales = st.sidebar.slider('JP_Sales', 0, 40, 1)
+    Other_Sales = st.sidebar.slider('Other_Sales', 0, 40, 1)
 
-    user_report_data = {
-        'NA_Sales': NA_Sales,
-        'EU_Sales': EU_Sales,
-        'JP_Sales': JP_Sales
+    # Create a dictionary with dummy values for unused features
+    dummy_data = {
+        # Assuming the model expects 63 features
+        'Unused_' + str(i): 0 for i in range(59)
     }
 
-    report_data = pd.DataFrame(user_report_data, index=[0])
+    # Update the dictionary with the user input for relevant features
+    dummy_data.update({
+        'NA_Sales': NA_Sales,
+        'EU_Sales': EU_Sales,
+        'JP_Sales': JP_Sales,
+        'Other_Sales': Other_Sales
+    })
+
+    # Create a DataFrame from the dictionary
+    report_data = pd.DataFrame(dummy_data, index=[0])
     return report_data
 
 
 user_data = user_report()
 st.header('Data')
-st.write(user_data)
+columns_to_display = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+st.write(user_data[columns_to_display])
 
 Global_Sales = model.predict(user_data)
-st.subheader('NA_Sales')
-st.subheader('$'+str(np.round(Global_Sales[0], 2)))
+st.subheader('Predicted Global Sales')
+st.subheader('$' + str(np.round(np.exp(Global_Sales[0]), 2)))
